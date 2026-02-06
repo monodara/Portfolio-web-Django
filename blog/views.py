5c4b078f1e0017bcc51707cb7fb2a83d68daa731
Yuan_Lu <642649909@qq.com>
2026-02-06 11:06:21 +0200

    Fix post_new function.

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,11 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
     else:
